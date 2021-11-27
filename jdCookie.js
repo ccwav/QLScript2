f41d79ac0d77682dd3b8f30f50d533e5c37840e0
ccwav <ccwav@163.com>
2021-11-28 00:26:26 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -24,8 +24,7 @@ if (JSON.stringify(process.env).indexOf('GITHUB')>-1) {
   })()
 }
 CookieJDs = [...new Set(CookieJDs.filter(item => !!item))]
-console.log(`\n====================共${CookieJDs.length}个京东账号Cookie=========\n`);
-console.log(`==================脚本执行- 北京时间(UTC+8)：${new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000).toLocaleString()}=====================\n`)
+console.log(`\n===============共${CookieJDs.length}个京东账号Cookie===============\n`);
 if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {};
 for (let i = 0; i < CookieJDs.length; i++) {
   if (!CookieJDs[i].match(/pt_pin=(.+?);/) || !CookieJDs[i].match(/pt_key=(.+?);/)) console.log(`\n提示:京东cookie 【${CookieJDs[i]}】填写不规范,可能会影响部分脚本正常使用。正确格式为: pt_key=xxx;pt_pin=xxx;（分号;不可少）\n`);

d10f4b0ee2434e03be7b54e88c2de11928aaffd1
ccwav <ccwav@163.com>
2022-04-06 12:21:02 +0800

    领现金Update,变量统一改为PandaToken

--- ModScript/jd_gua_cleancart_Panda.js
+++ ModScript/jd_gua_cleancart_Panda.js
@@ -6,7 +6,7 @@
 需要算法支持
 默认：不执行 如需要请添加环境变量
 gua_cleancart_Run="true"
-gua_cleancart_PandaToken="" # PanDaToken
+PandaToken="" # PanDaToken
 
 ——————————————
 1.@&@ 前面加数字 指定账号pin
@@ -57,14 +57,14 @@ if ($.isNode()) {
 
 message = ''
 
-jdPandaToken = $.isNode() ? (process.env.gua_cleancart_PandaToken ? process.env.gua_cleancart_PandaToken : `${jdPandaToken}`) : ($.getdata('gua_cleancart_PandaToken') ? $.getdata('gua_cleancart_PandaToken') : `${jdPandaToken}`);
+jdPandaToken = $.isNode() ? (process.env.PandaToken ? process.env.PandaToken : `${jdPandaToken}`) : ($.getdata('PandaToken') ? $.getdata('PandaToken') : `${jdPandaToken}`);
 
 cleancartRun = $.isNode() ? (process.env.gua_cleancart_Run ? process.env.gua_cleancart_Run : `${cleancartRun}`) : ($.getdata('gua_cleancart_Run') ? $.getdata('gua_cleancart_Run') : `${cleancartRun}`);
 
 cleancartProducts = $.isNode() ? (process.env.gua_cleancart_products ? process.env.gua_cleancart_products : `${cleancartProducts}`) : ($.getdata('gua_cleancart_products') ? $.getdata('gua_cleancart_products') : `${cleancartProducts}`);
 
 if (!jdPandaToken) {
-    console.log('请填写Panda获取的Token,变量是gua_cleancart_PandaToken');
+    console.log('请填写Panda获取的Token,变量是PandaToken');
 	return;
 }
 let productsArr = []

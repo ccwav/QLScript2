556369910dbce73c890d1c28b669ce054301588a
ccwav <ccwav@163.com>
2022-05-11 16:18:01 +0800

    增加汪汪赛跑金额

--- bot_jd_bean_change.js
+++ bot_jd_bean_change.js
@@ -187,6 +187,13 @@ if(DisableIndex!=-1){
 	EnableChaQuan=false
 }
 
+//汪汪赛跑
+let EnableJoyRun=true;
+DisableIndex=strDisableList.findIndex((item) => item === "汪汪赛跑");
+if(DisableIndex!=-1){
+	console.log("检测到设定关闭汪汪赛跑查询");
+	EnableJoyRun=false
+}
 
 !(async() => {
     if (!cookiesArr[intcheckckseq]) {
@@ -248,6 +255,7 @@ if(DisableIndex!=-1){
         $.YunFeiTitle2 = "";
         $.YunFeiQuan2 = 0;
         $.YunFeiQuanEndTime2 = "";
+		$.JoyRunningAmount = "";
         TempBaipiao = "";
         strGuoqi = "";
 		await Promise.all([
@@ -273,7 +281,8 @@ if(DisableIndex!=-1){
 		         getJxFactory(), //京喜工厂
 		         getDdFactoryInfo(), // 京东工厂
 		         jdCash(), //领现金
-		         GetJxBeaninfo() //喜豆查询		         
+		         GetJxBeaninfo(), //喜豆查询	
+				 GetJoyRuninginfo() //汪汪赛跑
 		     ])
 
         await showMsg();        
@@ -386,18 +395,24 @@ async function showMsg() {
 		ReturnMessage += `【京东秒杀】${$.JdMsScore}币(≈${($.JdMsScore / 1000).toFixed(2)}元)\n`;
 	}
 
-	if ($.joylevel || $.jdCash) {
+	if ($.joylevel || $.jdCash || $.JoyRunningAmount) {
 		ReturnMessage += `【其他信息】`;
 		if ($.joylevel) {
-			ReturnMessage += `汪汪:${$.joylevel}级`;
-			if ($.jdCash) {
-				ReturnMessage += ",";
-			}
+			ReturnMessage += `汪汪:${$.joylevel}级`;			
 		}
 		if ($.jdCash) {
+			if ($.joylevel) {
+				ReturnMessage += ",";
+			}			
 			ReturnMessage += `领现金:${$.jdCash}元`;
 		}
-
+		if ($.JoyRunningAmount) {
+			if ($.joylevel || $.jdCash) {
+				ReturnMessage += ",";
+			}			
+			ReturnMessage += `汪汪赛跑:${$.JoyRunningAmount}元`;
+		}
+		
 		ReturnMessage += `\n`;
 
 	}
@@ -1538,7 +1553,7 @@ async function JxmcGetRequest() {
 }
 
 // 惊喜工厂信息查询
-function getJxFactory() {
+async function getJxFactory() {
 	if (!EnableJxGC)
 		return;
 	return new Promise(async resolve => {
@@ -1909,6 +1924,58 @@ async function jxbean() {
 
 }
 
+
+function GetJoyRuninginfo() {
+	if (!EnableJoyRun)
+		return;
+	
+    const headers = {
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+        "Connection": "keep-alive",
+        "Content-Length": "376",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Cookie": cookie,
+        "Host": "api.m.jd.com",
+        "Origin": "https://joypark.jd.com",
+        "Referer": "https://joypark.jd.com/",
+        "User-Agent": $.UA
+		}
+	var DateToday = new Date();
+	const body = {
+        'linkId': 'L-sOanK_5RJCz7I314FpnQ',
+		'isFromJoyPark':true,
+		'joyLinkId':'LsQNxL7iWDlXUs6cFl-AAg'
+    };
+    const options = {
+        url: `https://api.m.jd.com/?functionId=runningPageHome&body=${encodeURIComponent(JSON.stringify(body))}&t=${DateToday.getTime()}&appid=activities_platform&client=ios&clientVersion=3.8.12`,
+        headers,
+    }
+    return new Promise(resolve => {
+        $.get(options, (err, resp, data) => {
+            try {
+                if (err) {
+                    console.log(`${JSON.stringify(err)}`)
+                    console.log(`${$.name} API请求失败，请检查网路重试`)
+                } else {
+                    if (data) {
+						//console.log(data);
+                        data = JSON.parse(data);
+                        if (data.data.runningHomeInfo.prizeValue) {
+							$.JoyRunningAmount=data.data.runningHomeInfo.prizeValue * 1;							
+						}
+                    }
+                }
+            } catch (e) {
+                $.logErr(e, resp)
+            }
+            finally {
+                resolve(data)
+            }
+        })
+    })
+}
 
 	
 function randomString(e) {

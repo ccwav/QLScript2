23679becb6854cc0007de6e3eaf2034dcd17d9cd
ccwav <ccwav@163.com>
2022-08-16 21:35:34 +0800

    增加Plus综合分

--- bot_jd_bean_change_QL.js
+++ bot_jd_bean_change_QL.js
@@ -264,6 +264,7 @@ if(DisableIndex!=-1){
         $.YunFeiQuanEndTime2 = "";
 		$.JoyRunningAmount = "";
 		$.ECardinfo = "";
+		$.PlustotalScore=0;
         TempBaipiao = "";
         strGuoqi = "";
 
@@ -292,7 +293,8 @@ if(DisableIndex!=-1){
 		         jdCash(), //领现金
 		         GetJxBeaninfo(), //喜豆查询	
 				 GetJoyRuninginfo(), //汪汪赛跑 
-				 CheckEcard() //E卡查询
+				 CheckEcard(), //E卡查询
+				 queryScores() 
 		     ])
 
         await showMsg();        
@@ -317,7 +319,8 @@ async function showMsg() {
 		ReturnMessageTitle = `【账号🆔${(intcheckckseq+1)}】`+strRemark+`\n`;
 	else
 		ReturnMessageTitle = `【账号${(intcheckckseq+1)}收支情况】\n`;
-
+	
+		
 	if ($.levelName || $.JingXiang){
 		ReturnMessage += `【账号信息】`;
 		if ($.levelName) {
@@ -339,10 +342,14 @@ async function showMsg() {
 			if ($.levelName == "铜牌")
 				$.levelName = `🥉铜牌`;
 
-			if ($.isPlusVip == 1)
+			if ($.isPlusVip == 1){
 				ReturnMessage += `${$.levelName}Plus`;
+				if($.PlustotalScore)
+					ReturnMessage+=`(${$.PlustotalScore}分)`
+			}				
 			else
-				ReturnMessage += `${$.levelName}会员`;
+				ReturnMessage += `${$.levelName}会员`;			
+			
 		}
 
 		if ($.JingXiang){
@@ -351,6 +358,7 @@ async function showMsg() {
 			}
 			ReturnMessage += `${$.JingXiang}`;
 		}
+		
 		ReturnMessage +=`\n`;
 	}
 	
@@ -2303,6 +2311,31 @@ function GetDateTime(date) {
 		timeString += date.getSeconds();
 
 	return timeString;
+}
+async function queryScores() {
+	if ($.isPlusVip != 1)
+		return
+    let res = ''
+    let url = {
+      url: `https://rsp.jd.com/windControl/queryScore/v1?lt=m&an=plus.mobile&stamp=${Date.now()}`,
+      headers: {
+        'Cookie': cookie,
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; Redmi Note 8 Pro Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045715 Mobile Safari/537.36',
+        'Referer': 'https://plus.m.jd.com/rights/windControl'
+      }
+    };
+	
+    $.get(url, async (err, resp, data) => {
+      try {
+        const result = JSON.parse(data)
+        if (result.code == 1000) {
+		  $.PlustotalScore=result.rs.userSynthesizeScore.totalScore;
+        } 
+      } catch (e) {
+        $.logErr(e, resp);
+      }
+    })
+  
 }
 
 // prettier-ignore
